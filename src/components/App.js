288bfef030402c1799a17349bf5733af9b747483
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-09-30 17:02:48 +0300

    lab passed

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,20 @@ import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
-console.log(user);
+
+
+
 
 function App() {
+  console.log(user.links);
+  
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color={user.color} city={user.city} name={user.name}/>
+      {/* <About bio={user.bio?user.bio:"I made this?"} links={user.links} /> */}
+      <About bio={user.bio?user.bio='this is bio data':"I made this?"} links={user.links} />
+
     </div>
   );
 }
